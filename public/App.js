92bd7d39d63c9aa4e2d7512e1a0cbcdc7c22cc76
Shongsu <chinhouu1992@gmail.com>
2019-07-17 20:56:30 -0400

    automation

--- public/App.js
+++ public/App.js
@@ -1,4 +1,11 @@
-const element = React.createElement("div", {
+"use strict";
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'TEST'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello ".concat(c, "!");
+});
+var message = helloContinents.join(' ');
+var element = React.createElement("div", {
   title: "Outer div"
-}, React.createElement("h1", null, "Hello World!"));
+}, React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
